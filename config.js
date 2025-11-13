ccaabf465f1d19d3ceed0ae3354ae2fa00e59ce6
Thomas <154765054+thomasvmc@users.noreply.github.com>
2025-11-13 16:15:04 +0100

    update config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
-  token: process.env.DISCORD_BOT_TOKEN || process.env.DISCORD_TOKEN,
-  clientId: process.env.DISCORD_CLIENT_ID || process.env.DISCORD_APPLICATION_ID,
-  guildId: process.env.DISCORD_GUILD_ID
+  gemini_token: process.env.GEMINI_API_TOKEN,
+  token: process.env.DISCORD_BOT_TOKEN,
+  clientId: process.env.DISCORD_CLIENT_ID,
 };
